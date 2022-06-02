1f216b674449d6eee9b8c9bfbb44919d7e59e102
michael1231 <michaellim1231@gmail.com>
2022-06-02 19:14:57 +0700

    Added onBoarding

--- AFL4_Michael/ContentView.swift
+++ AFL4_Michael/ContentView.swift
@@ -9,9 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 16){
+            RoundedRectangle(cornerRadius: 25)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.mint]), startPoint:  .leading, endPoint: .trailing))
+                .frame(height: 60)
+                .mask(Text("To do list apps")
+                    .bold()
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading))
+            Label(
+                title:{ Text("Add item to your list")
+                    .font(.system(size: 20))},
+                icon:{ Image(systemName: "pencil.circle")})
+            .font(.system(size: 32))
             .padding()
+            Label(
+                title:{ Text("Edit your list")
+                    .font(.system(size: 20))},
+                icon:{ Image(systemName: "square.and.pencil")})
+                .font(.system(size: 32))
+                .padding()
+            Label(
+                title:{ Text("Prioritize your list")
+                    .font(.system(size: 20))},
+                icon:{ Image(systemName: "list.bullet")})
+                .font(.system(size: 32))
+                .padding()
+            Button(action: {
+                
+            },label: {
+                Text("Get Started")
+                    .foregroundColor(.mint)
+                    .font(.headline)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(.black)
+                    .cornerRadius(20)
+                
+            })
+
+                   
+        }
+        .padding(40)
+        .background(.white)
+        .mask(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10), style: .continuous))
+        .padding(20)
+        .shadow(color: Color.black, radius: 5, x: 0.2, y: 0.2)
+    
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {

17bd93964624c1e62b80b1b2c65785c7ebd13534
michael1231 <michaellim1231@gmail.com>
2022-06-02 19:05:54 +0700

    Initial Commit

---
+++ AFL4_Michael/AFL4_MichaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4_MichaelApp.swift
+//  AFL4_Michael
+//
+//  Created by Michael on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4_MichaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL4_Michael/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AFL4_Michael
+//
+//  Created by Michael on 02/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

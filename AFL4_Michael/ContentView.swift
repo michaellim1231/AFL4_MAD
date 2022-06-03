7caab7186f474c1adc4f46107e2c7dac1ce0a95a
michael1231 <michaellim1231@gmail.com>
2022-06-03 09:44:17 +0700

    adding priority low,medium,high

--- AFL4_Michael/ContentView.swift
+++ AFL4_Michael/ContentView.swift
@@ -7,14 +7,49 @@
 
 import SwiftUI
 
+enum Priority: String, Identifiable, CaseIterable {
+    
+    var id: UUID {
+        return UUID()
+    }
+    
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+}
+
+extension Priority {
+    
+    var title: String{
+        switch self{
+        case .low:
+            return "Low"
+        case .medium:
+            return "Medium"
+        case .high:
+            return "High"
+        }
+    }
+}
 struct ContentView: View {
     @State var shouldShowOnboarding: Bool = true
-
+    
+    @State private var title: String = ""
+    
+    @State private var selectedPriority: Priority = .medium
+    
     var body: some View {
         NavigationView{
             VStack {
-                Text("Main Home")
+                TextField("Enter title", text: $title)
+                    .textFieldStyle(.roundedBorder)
+                Picker("Priority", selection: $selectedPriority) {
+                    ForEach(Priority.allCases) { priority in
+                        Text(priority.title).tag(priority)
+                    }
+                }.pickerStyle(.segmented)
             }
+            .padding()
             .navigationTitle("All Tasks")
         }
         .fullScreenCover(isPresented: $shouldShowOnboarding, content: { OnboardingView(shouldShowOnboarding: $shouldShowOnboarding)
